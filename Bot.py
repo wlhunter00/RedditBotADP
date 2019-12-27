ddf910ffbf9d98078676cbff8e0aafd24fa0c548
wlhunter00 <30416118+wlhunter00@users.noreply.github.com>
2019-12-28 00:04:02 +0100

    basis of logining into gdrive and accessing info in a google sheet done

--- Bot.py
+++ Bot.py
@@ -1,26 +1,67 @@
 # Imports
 import praw
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+
+# Main function that runs everything
+def main():
+    info = openFile('loginInfo.txt')
+    # reddit = loginReddit(info)
+    # subreddit = loadSubreddit(reddit, info[5])
+    loadGDrive(info[6])
+    # parseComments(subreddit)
+
 
 # Opening the file with the login info
-fd = open(r'loginInfo.txt', 'r')
-file = fd.read()
-info = file.split(';')
-print(info)
-fd.close()
+def openFile(filename):
+    fd = open(filename, 'r')
+    file = fd.read()
+    info = file.split(';')
+    print(info)
+    fd.close()
+    return info
 
-reddit = praw.Reddit(user_agent=info[0],
-                     client_id=info[1], client_secret=info[2],
-                     username=info[3], password=info[4])
 
-subreddit = reddit.subreddit('AskReddit')
-# for submission in subreddit.stream.submissions():
-for submission in subreddit.hot(limit=10):
-    print("title: ", submission.title.encode("utf-8"))
-    print("score: ", submission.score)
-    submission.comments.replace_more(limit=0)
-    for comment in submission.comments.list():
-        try:
-            print("comment by ", comment.author.name.encode("utf-8"),
-              ": ", comment.body.encode("utf-8"))
-        except:
-            print("error")
+# Loging into reddit
+def loginReddit(info):
+    return praw.Reddit(user_agent=info[0],
+                       client_id=info[1], client_secret=info[2],
+                       username=info[3], password=info[4])
+
+
+# load Subreddit
+def loadSubreddit(reddit, subreddit):
+    subreddit = reddit.subreddit(subreddit)
+
+
+# Load Google drive
+def loadGDrive(spreadsheet):
+    scope = ['https://spreadsheets.google.com/feeds',
+             'https://www.googleapis.com/auth/drive']
+    creds = ServiceAccountCredentials.from_json_keyfile_name('./client_secret.json', scope)
+    client = gspread.authorize(creds)
+
+    # Pick spreadsheet in the logininfo.txt
+    sheet = client.open(spreadsheet).sheet1
+
+    list_of_hashes = sheet.get_all_records()
+    print(list_of_hashes)
+
+
+# Parse the comments
+def parseComments(subreddit):
+    # for submission in subreddit.stream.submissions():
+    for submission in subreddit.hot(limit=10):
+        print("title: ", submission.title.encode("utf-8"))
+        print("score: ", submission.score)
+        submission.comments.replace_more(limit=0)
+        for comment in submission.comments.list():
+            try:
+                print("comment by ", comment.author.name.encode("utf-8"),
+                      ": ", comment.body.encode("utf-8"))
+            except:
+                print("error")
+
+if __name__ == "__main__":
+    main()
