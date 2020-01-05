63d4474021f6f6285eb9cf9ba9ff893d4a6a5859
wlhunter00 <30416118+wlhunter00@users.noreply.github.com>
2020-01-05 18:09:37 -0500

    have the ability to parse by username

--- Bot.py
+++ Bot.py
@@ -11,8 +11,8 @@ def main():
     info = openFile('C:\\Users\\William\\Documents\\GitHub\\RedditBotADP\\loginInfo.txt')
     reddit = loginReddit(info)
     subreddit = loadSubreddit(reddit, info[5])
-    listOfResponses = loadGDrive(info[6])
-    parseComments(subreddit, listOfResponses, info[3])
+    responses = loadGDrive(info[6], info[7])
+    scrollSubreddit(subreddit, responses, info[3])
 
 
 # Opening the file with the login info and other info needed for the process.
@@ -54,20 +54,29 @@ def loadSubreddit(reddit, subreddit):
 # To have the bot be able to access a file, share it with:
 # "redditbot@reddit-bot-263322.iam.gserviceaccount.com"
 # Input is the name of the spreadsheet.
-# Returns a list of dictionarys, where column 1 is the comment,
-# and column 2 is the response
-def loadGDrive(spreadsheet):
+# Returns 2 lists of dictionaries, where column 1 is the comment/user being
+# searched and column 2 is the response. The first element is number of
+# dictionaries, the second element for comment parsing, the third for user.
+def loadGDrive(spreadsheet, numberOfSheets):
     # Login to Google API using OAuth
     scope = ['https://spreadsheets.google.com/feeds',
              'https://www.googleapis.com/auth/drive']
     creds = ServiceAccountCredentials.from_json_keyfile_name('C:\\Users\\William\\Documents\\GitHub\\RedditBotADP\\client_secret.json', scope)
     client = gspread.authorize(creds)
-    # Pick spreadsheet in the logininfo.txt
-    sheet = client.open(spreadsheet).sheet1
-    # Return pairs of comments and responses.
-    list_of_hashes = sheet.get_all_records()
-    print(list_of_hashes)
-    return list_of_hashes
+    # Do these twice, the first sheet being the comment parsing
+    # The second sheet being user parsing
+    spreadsheetOpen = client.open(spreadsheet)
+    sheet1 = spreadsheetOpen.get_worksheet(0)
+    commentParseResponses = sheet1.get_all_records()
+    # Check in the loginInfo.txt document to see how many types of searches
+    # are doing.
+    if numberOfSheets == '2 sheets':
+        sheet2 = spreadsheetOpen.get_worksheet(1)
+        userParseResponses = sheet2.get_all_records()
+        # If two, return a list of both dictionaries.
+        return [2, commentParseResponses, userParseResponses]
+    # Else return the one dictionary
+    return [1, commentParseResponses]
 
 
 # Parse the comments in a subreddit, if the comment has a phrase we are looking
@@ -75,27 +84,54 @@ def loadGDrive(spreadsheet):
 # With this setup, the stream.submissions() will have the bot always being
 # Running, looking for any more inputs. Use subreddit.hot() or .new()
 # to have it run one time.
-def parseComments(subreddit, listOfResponses, botName):
+def scrollSubreddit(subreddit, responses, botName):
+    # Print responses from loadGDrive
+    print(responses)
+    # For each post in the subreddit
     for submission in subreddit.stream.submissions():
         print("title: ", submission.title.encode("utf-8"))
         submission.comments.replace_more(limit=0)
         for comment in submission.comments.list():
-            for response in listOfResponses:
+            # Commented = False prevents a comment about the user and comment.
+            commented = False
+            # By default run the comment parser
+            for response in responses[1]:
+                # If a match
                 if findWholeWord(response["Comment"])(comment.body):
-                    alreadyCommented = False
-                    if(comment.author.name != botName):
-                        for secondCheck in comment.replies:
-                            if(secondCheck.author.name == botName):
-                                print("Bot already commented")
-                                alreadyCommented = True
-                                break
-                        if not alreadyCommented:
-                            responseName = "Response " + str(random.randint(1, response['Number of Responses']))
-                            print("Match", response[responseName],
-                                  comment.body.encode("utf-8"))
-                            # comment.reply(response["Response"])
+                    # Respond to that comment
+                    respondToComment(comment, response, botName)
+                    commented = True
                     break
+            # If the post hasn't been commented, and there is a
+            # user sheet ready.
+            if ((commented == False) and (responses[0] > 1)):
+                for response in responses[2]:
+                    # Checks usernames
+                    if submission.author.name.lower() == response['Username'].lower():
+                        # Respond to that comment
+                        respondToComment(comment, response, botName)
 
 
+# Respond to a comment
+# Input is the comment object, the response object, and the name of the bot.
+def respondToComment(comment, response, botName):
+    # Check to make sure the bot isn't responding to itself.
+    if(comment.author.name != botName):
+        # Check the comment tree of replies for that comment to make sure that
+        # The bot hasn't already said something. Done to prevent repeats.
+        for secondCheck in comment.replies:
+            if(secondCheck.author.name == botName):
+                # If found just return and leave the function
+                print("Bot already commented")
+                return
+        # if not already commented, randomly pick which response to choose from
+        # This is done in the Number of Responses column in the google sheet.
+        responseName = "Response " + str(random.randint(1, response['Number of Responses']))
+        print("Match: Found -", comment.author.name, ":",
+              comment.body.encode("utf-8"), "- Response:", response[responseName])
+        comment.reply(response[responseName])
+
+
+# Run main function
 if __name__ == "__main__":
     main()
--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # RedditBotADP
-Reddit Bot for Rush Subreddit
+## Introduction
+This is a reddit bot that I created that will parse through all of the comments in a subreddit, and reply to them based on certain keywords or certain users that did the commenting. The list of users and words to look for are stored within a Google Sheets document.
+This bot can be scaled to look for many different things in the comment, such as comment score, and the type of reply can also be scaled to be more complex.
+
+This reddit bot can be used for any subreddit, and look for any words/users. It can be scaled however the user my want it.
+
+#### Process
+1. Load all information needed from a text document.
+2. Login to Reddit's API based on credentials given.
+3. Load the subreddit that we want to look at.
+4. Login to Google Drive's API and pull up the spreadsheet that we want to look at. Put all the information from the two (or one) workbooks into objects, and return them.
+5. Go through every post in the subreddit and look through it's comments. Parse each comment to see if it has any of the keywords that we are looking for.
+6. If there is a match, then check to see if the Bot was the one that commented, or if that bot has ever responded in that chain. Then it will reply to the comment.
+7. Check to see the username of the commenter. If it is in the list of users we want to reply to, go to step 6.
+
+## Table of Contents
+* [Introduction](#introduction)
+* [Table of Contents](#table-of-contents)
+* [Project Status](#project-status)
+  * [Recently added](#recently-added)
+  * [To Do](#to-do)
+* [Walkthroughs](#Walkthroughs)
+  * [Functions Walkthrough](#functions-walkthrough)
+  * [Text file Walkthrough](#textfile-walkthrough)
+  * [Spreadsheet Walkthrough](#spreadsheet-walkthrough)
+* [Technologies](#technologies)
+* [Installation](#installation)
+* [Typical Errors](#typical-errors)
+
+## Project Status
+###### **Version 1.2**
+
+Stable build is currently ready.
+
+###### **Recently Added**
+- [X] Created this README as a guide to the application.
+- [X] Allowed for users to be parsed
+
+#### To Do:
+- [ ] Allow the responses from the bot to interact with the comment.
+- [ ] Create "Smart" responses that can answer questions/
